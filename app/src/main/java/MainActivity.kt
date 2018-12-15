319bdefd1a16c01c060e3045b874004af67133c1
Minsuk Eom <boxresin@gmail.com>
2018-12-15 22:44:22 +0900

    첫 번째 커밋

---
+++ app/src/main/java/MainActivity.kt
@@ -0,0 +1,12 @@
+package winapi251.app.schoolmeal
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/** 메인 액티비티 */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
