1586998970b63c3a66ab179edfab25c46529284a
Minsuk Eom <boxresin@gmail.com>
2018-12-22 13:37:40 +0900

    appContext 전역변수 추가 및 초기화 close #16

--- app/src/main/java/Global.kt
+++ app/src/main/java/Global.kt
@@ -1,2 +1,7 @@
 /** 전역변수 파일 */
 package winapi251.app.schoolmeal
+
+import android.content.Context
+
+// 앱 컨텍스트
+lateinit var appContext: Context
--- app/src/main/java/SchoolMealApplication.kt
+++ app/src/main/java/SchoolMealApplication.kt
@@ -3,4 +3,11 @@ package winapi251.app.schoolmeal
 import android.app.Application
 
 /** 커스텀 애플리케이션 */
-class SchoolMealApplication : Application()
+class SchoolMealApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // 전역변수 앱 컨텍스트 초기화
+        appContext = this.applicationContext
+    }
+}
