204d85d2bcfda25bf71ec341ca5d6ede6d1ba232
Minsuk Eom <boxresin@gmail.com>
2018-12-25 20:46:12 +0900

    Catch All 유틸 추가 #19

--- app/src/main/java/util/Kotlins.kt
+++ app/src/main/java/util/Kotlins.kt
@@ -1,2 +1,10 @@
 /** 코틀린 관련 유틸리티 */
 package winapi251.app.schoolmeal.util
+
+/** [block]을 실행하면서 발생하는 모든 예외를 삼킨다. */
+inline fun safe(crossinline block: () -> Unit) {
+    try {
+        block()
+    } catch (ignored: Throwable) {
+    }
+}
